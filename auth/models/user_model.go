9b5a3182747660d56d1eeb2405932b08b58bdea9
BGabor01 <busnes14@gmail.com>
2024-12-05 21:25:45 +0100

    user_model: User model

---
+++ auth/models/user_model.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct{
+	gorm.Model
+	Name  string `json:"name" gorm:"unique"`
+	Password  string `json:"password"`
+	Email string `json:"email" binding:"required,email"`
+}
